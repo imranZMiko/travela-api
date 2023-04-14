e4f5fa8384a125f2e329fd951f0197c0cf772809
imranZMiko <imran.zh.miko@gmail.com>
2023-04-15 01:05:48 +0600

    Added models

---
+++ travela_api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class User(models.Model):
+    userID = models.CharField(primary_key=True, max_length=128)
+    userName = models.CharField(max_length=50)
+    userImage = models.ImageField(upload_to='userImages/')
+
+class Trip(models.Model):
+    owner = models.ForeignKey(User, related_name='trips', on_delete=models.CASCADE)
+    tripName = models.CharField(max_length=80)
+    tripImage = models.ImageField(upload_to='tripImages/')
+    startDate = models.DateField()
+    endDate = models.DateField()
+    sharedUsers = models.ManyToManyField(User)
+
+class ItineraryEntry(models.Model):
+    trip = models.ForeignKey(Trip, related_name='itinerary_entries', on_delete=models.CASCADE)
+    dateTime = models.DateTimeField()
+    description = models.CharField(max_length=500)
+    location = models.CharField(max_length=80)
--- travela_api/settings.py
+++ travela_api/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travela_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

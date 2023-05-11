3c6bc778412e153ffb2bce7ce1b1e008b400d2ef
imranZMiko <imran.zh.miko@gmail.com>
2023-05-12 02:47:57 +0600

    Adjusted scraping

--- travela_api/scraper.py
+++ travela_api/scraper.py
@@ -49,7 +49,8 @@ def getDestinations(search_term):
         image = re.findall(r'\((.*?)\)', image)
         image = image[0]
         tag = hotel.find("div", class_="thumbnail").find("span", class_="thumbnail-overlay-tag").text.strip()
-        result.append({"name":name, "address":address, "image":[image], "tag":tag, "description":None})
+        if tag in ["Hotels", "Castles", "Religious Sites", "Ancient Ruins", "Historic Sites","Bodies of Water","Mountains","Forests","Parks","Activities","Architectural Buildings","Points of Interest & Landmarks","Scenic Walking Areas","Restaurants"]:
+            result.append({"name":name, "address":address, "image":[image], "tag":tag, "description":None})
         # print(f"Name: {name}\nAddress: {address}\nImage: {image}")
 
     # # close the browser
@@ -76,38 +77,131 @@ def getDestinationDetails(search_term):
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
-    # # find all the hotels on the search results page
     hotel = soup.find("div", class_="prw_search_search_result_poi")
     name = hotel.find("div", class_="result-title").text.strip()
     address = hotel.find("div", class_="address-text").text.strip()
     tag = hotel.find("div", class_="thumbnail").find("span", class_="thumbnail-overlay-tag").text.strip()
 
-    returned = driver.execute_script("return document.querySelector('[data-widget-type=\"TOP_RESULT\"]').querySelector('.result-title').getAttribute(\"onclick\")")
-    returned = re.findall("[^']*\.html", returned)
+    try:
+        returned = driver.execute_script("return document.querySelector('[data-widget-type=\"TOP_RESULT\"]').querySelector('.result-title').getAttribute(\"onclick\")")
+        returned = re.findall("[^']*\.html", returned)
+    except:
+        returned = driver.execute_script("return document.querySelector('.prw_search_search_result_poi').querySelector('.result-title').getAttribute(\"onclick\")")
+        returned = re.findall("[^']*\.html", returned)
+    
     returned = returned[0]
 
     driver.get(f"https://www.tripadvisor.com{returned}")
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "HEADING"))
-    )
+    if tag == "Hotels":
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "HEADING"))
+        )
 
-    # create a Beautiful Soup object from the search results page
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+        # create a Beautiful Soup object from the search results page
+        soup = BeautifulSoup(driver.page_source, "html.parser")
 
-    description = soup.find("div", class_="fIrGe _T").text.strip()
-    imageUrls = []
-    images = soup.find_all("img")
-    for image in images:
-        url = image.get("srcset")
-        if url is not None:
-            url = url.split()[0]
-            imageUrls.append(url)
+        try:
+            description = soup.find("div", class_="fIrGe _T").text.strip()
+        except:
+            description = None
+        imageUrls = []
+        images = soup.find("div", class_="rXyAo").find_all("img")
+        for image in images:
+            url = image.get("srcset")
+            if url is not None:
+                url = url.split()[0]
+                url = re.sub(r'w=\d+0', 'w=1800', url)
+                imageUrls.append(url)
+    elif tag in ["Castles", "Religious Sites", "Ancient Ruins", "Historic Sites","Bodies of Water","Mountains","Forests","Parks"] :
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "eIegw"))
+        )
 
-    # print(f"Name: {name}\nAddress: {address}\nTag: {tag}\nImage: {imageUrls}\nDescription: {description}\n")
+        # create a Beautiful Soup object from the search results page
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        try:
+            description = soup.find("div", class_="_d MJ").find("div", class_="KxBGd").text.strip()
+        except:
+            description = None
+        imageUrls = []
+        images = soup.find("div", class_="yNgTB").find_all("img")
+        for image in images:
+            url = image.get("src")
+            if url is not None:
+                url = url.split()[0]
+                url = re.sub(r'w=\d+0', 'w=1800', url)
+                imageUrls.append(url)
+    elif tag == "Activities":
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "EVnyE"))
+        )
+
+        # create a Beautiful Soup object from the search results page
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        try:
+            temp = soup.find("div", class_="biGQs _P pZUbB roAGK KxBGd")
+            if temp is None:
+                temp = soup.find("div", class_="biGQs _P pZUbB KxBGd")
+                description = temp.find("div", class_="_d").text.strip()
+            else:
+                description = temp.find("div", class_="_d").text.strip()
+        except:
+            description = None
+        
+        imageUrls = []
+        images = soup.find_all("img")
+        for image in images:
+            url = image.get("src")
+            if url.startswith("https://media-cdn.tripadvisor.com") or url.startswith("https://dynamic-media-cdn.tripadvisor.com/media/"):
+                url = url.split()[0]
+                url = re.sub(r'w=\d+0', 'w=1800', url)
+                imageUrls.append(url)
+    elif tag == "Restaurants":
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "EVnyE"))
+        )
+
+        # create a Beautiful Soup object from the search results page
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        description = None
+
+        imageUrls = []
+        images = soup.find("div", class_="mosaic_photos").find_all("img")
+        for image in images:
+            url = image.get("data-lazyurl")
+            if url is not None:
+                if url.startswith("https://media-cdn.tripadvisor.com") or url.startswith("https://dynamic-media-cdn.tripadvisor.com/media/"):
+                    url = url.split()[0]
+                    url = re.sub(r'w=\d+0', 'w=1800', url)
+                    imageUrls.append(url)
+    #yNgTB
+    elif tag in ["Architectural Buildings","Points of Interest & Landmarks","Scenic Walking Areas"]:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "EVnyE"))
+        )
+
+        # create a Beautiful Soup object from the search results page
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        description = None
+
+        imageUrls = []
+        images = soup.find("div", class_="yNgTB").find_all("img")
+        for image in images:
+            url = image.get("src")
+            if url is not None:
+                if url.startswith("https://media-cdn.tripadvisor.com") or url.startswith("https://dynamic-media-cdn.tripadvisor.com/media/"):
+                    url = url.split()[0]
+                    url = re.sub(r'w=\d+0', 'w=1800', url)
+                    imageUrls.append(url)
+
     result = {"name":name, "address":address, "image":imageUrls, "tag":tag, "description":description}
 
-    # # close the browser
+    # close the browser
     driver.quit()
 
     # print(result)
@@ -122,7 +216,7 @@ def getDestinationLocation(search_term):
     driver = webdriver.Firefox(options=firefox_options)
 
     # navigate to the TripAdvisor homepage
-    driver.get(f"https://www.google.com/maps/")
+    driver.get(f"https://www.google.com/maps?hl=en")
 
     # wait for the search results page to load
     WebDriverWait(driver, 10).until(
@@ -138,7 +232,7 @@ def getDestinationLocation(search_term):
     driver.execute_script("return document.querySelector(\"button[id='searchbox-searchbutton']\").click()")
 
     WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "DUwDvf"))
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".k7jAl.lJ3Kh.miFGmb"))
     )
 
     time.sleep(1)
